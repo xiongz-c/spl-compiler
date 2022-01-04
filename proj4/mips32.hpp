4751f3f18646dfb0eb7a08acf31a04f841fc9051
EricLee543 <408228228@qq.com>
2022-01-04 13:31:18 +0800

    add addree desc

--- proj4/mips32.hpp
+++ proj4/mips32.hpp
@@ -35,6 +35,51 @@ enum MipsOp {
     MIPS_SYSCALL
 };
 
+typedef enum {
+    zero, at, v0, v1, a0, a1, a2, a3,
+    t0, t1, t2, t3, t4, t5, t6, t7,
+    s0, s1, s2, s3, s4, s5, s6, s7,
+    t8, t9, k0, k1, gp, sp, fp, ra, NUM_REGS, NO_REG
+} Register;
+
+unordered_map<Register, string> regToStr = {
+        {zero,"zero"},
+        {at,"at"},
+        {v0,"v0"},{v1,"v1"},
+        {a0,"a0"}, {a1,"a1"}, {a2,"a2"}, {a3,"a3"},
+        {t0,"t0"}, {t1,"t1"}, {t2,"t2"}, {t3,"t3"}, {t4,"t4"}, {t5,"t5"}, {t6,"t6"}, {t7,"t7"},
+        {s0,"s0"}, {s1,"s1"}, {s2,"s2"}, {s3,"s3"}, {s4,"s4"}, {s5,"s5"}, {s6,"s6"}, {s7,"s7"},
+        {t8,"t8"}, {t9,"t9"}, {k0,"k0"}, {k1,"k1"}, {gp,"gp"}, {sp,"sp"}, {fp,"fp"}, {ra,"ra"},
+};
+
+Register localAllocate();
+
+struct AddressDesc{
+    string name;
+    Register reg_num; // the number of register eg:  t0 is 8
+    int offset; // offset to $fp
+    bool isPlus;
+
+    AddressDesc(string name, Register reg, int offset, bool isPlus) {
+        this->name = name;
+        this->reg_num = reg;
+        this->offset = offset;
+        this->isPlus = isPlus;
+    }
+
+};
+
+unordered_map<string, AddressDesc*> nameToAddress;
+
+struct RegDesc {    // the register descriptor
+    string name;
+    string var;
+    bool dirty; // value updated but not stored
+    bool isAllocated = false;
+    AddressDesc * adr = NULL;
+    /* add other fields as you need */
+} regs[NUM_REGS];
+
 class Inst {
 public:
     string res;
@@ -42,7 +87,7 @@ public:
     string arg2;
     MipsOp op;
 
-    stirng debug_info; // comment for debug
+    string debug_info; // comment for debug
 
 
     Inst(MipsOp op){
@@ -69,7 +114,7 @@ public:
 
     void print_inst() {
         if (this->op != MIPS_LABEL) {
-            cout << "\t";
+            cout << "  ";
         }
         cout << this->to_string();
         if (!debug_info.empty()) {
@@ -144,12 +189,16 @@ public:
             it->print_inst();
         }
     }
-};
 
+    void put_Inst(Inst * ins){
+        instructions.push_back(ins);
+    }
+};
 
 class Block {
 public:
     vector<Tac*> ir;
+    vector<Tac*> param_tac_list; //如果block是一个函数，需要计算参数的个数，确定fp的offset
     Mips* mips;
 
     Block(Tac* ld) {this->ir.push_back(ld);}
@@ -165,13 +214,96 @@ public:
     void set_mips(Mips* m) {
         this->mips = m;
     }
+
+    // 生成callee
+    void generateFunction(Mips* mips, Tac* tac);
+    void generateAssign(Mips* mips, Tac* tac);
+
     // 向当前所属的mips类添加指令
     void gen_code() {
+        //this->print();
+        this->analysis();
+        auto itr = ir.begin();
+        while(itr != ir.end()){
+            Tac* tac = *(itr);
+            if(tac->tac_type == Tac::FUNC){
+                generateFunction(this->mips,tac);
+            }else if(tac->tac_type == Tac::ASSIGN){
+                generateAssign(this->mips,tac);
+            }
+            itr++;
+        }
+    }
+
+    void analysis(){
+        auto itor = ir.end();
+        while(itor != ir.begin()){
+            itor--;
+            Tac * tac = *(itor);
+            if(tac->tac_type == Tac::PARAM){
+                param_tac_list.push_back(tac);
+            }
+        }
 
     }
 };
 
 list<Block*> block_list;
+
+Register localAllocate(){
+    for(Register tmp = t0 ; tmp <= t9 ; tmp = (Register)(tmp+1)){
+        if(!regs[tmp].isAllocated){
+            regs[tmp].isAllocated = true;
+            return tmp;
+        }
+    }
+    return NO_REG;
+}
+
+Register argAllocate(){
+    for(Register tmp = a0 ; tmp <= a3 ; tmp = (Register)(tmp+1)){
+        if(!regs[tmp].isAllocated){
+            regs[tmp].isAllocated = true;
+            return tmp;
+        }
+    }
+    return NO_REG;
+}
+
+
+void Block::generateFunction(Mips* mips, Tac* tac){
+    Inst * ins = new Inst(MIPS_LABEL,tac->operands[RESULT].c_str());
+    mips->put_Inst(ins);
+    mips->put_Inst(new Inst(MIPS_MOVE, "$fp", "$sp"));
+    int param_size = this->param_tac_list.size();
+    int offset_cnt = param_size + 2;
+    for (auto item = this->param_tac_list.begin(); item != this->param_tac_list.end() ; ++item) {
+        Register tmp = argAllocate();
+        offset_cnt--;
+        Tac * tac = (*item);
+        string name = tac->operands[RESULT];
+        if(regs[tmp].adr == NULL){
+            regs[tmp].isAllocated = true;
+            auto it = nameToAddress.find(name);
+            if( it != nameToAddress.end()){
+
+            }else{
+                AddressDesc* address = new AddressDesc(name,tmp,offset_cnt*4, true);
+                nameToAddress.insert(make_pair(name,address));
+                regs[tmp].adr = address;
+                this->mips->put_Inst(new Inst(MIPS_LW,regToStr[tmp],"$fp", to_string(address->offset)));
+            }
+        }
+
+    }
+
+
+}
+
+void Block::generateAssign(Mips* mips, Tac* tac){
+    cout << tac->operands[RESULT] << " is assign " << endl;
+    cout << tac->operands[ARG1] << " to be assigned " << endl;
+}
 
 //JUMP和LABEL
 bool is_leader(Tac* tac) {
@@ -259,21 +391,6 @@ void emit_write_function(){
     _mips_iprintf("jr $ra");
 }
 
-
-typedef enum {
-    zero, at, v0, v1, a0, a1, a2, a3,
-    t0, t1, t2, t3, t4, t5, t6, t7,
-    s0, s1, s2, s3, s4, s5, s6, s7,
-    t8, t9, k0, k1, gp, sp, fp, ra, NUM_REGS
-} Register;
-
-
-struct RegDesc {    // the register descriptor
-    string name;
-    string var;
-    bool dirty; // value updated but not stored
-    /* add other fields as you need */
-} regs[NUM_REGS];
 
 
 struct VarDesc {    // the variable descriptor
